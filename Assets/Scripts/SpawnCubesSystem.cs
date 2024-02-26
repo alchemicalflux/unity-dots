dcc19c9a287d47c1422c9895d9cc364d0c587550
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2024-02-25 22:31:20 -0800

    fix: Implement method to create large entity groups

--- Assets/Scripts/SpawnCubesSystem.cs
+++ Assets/Scripts/SpawnCubesSystem.cs
@@ -5,9 +5,10 @@
   Copyright:      
 
   Last commit by: alchemicalflux 
-  Last commit at: 2024-02-25 22:04:46 
+  Last commit at: 2024-02-25 22:31:20 
 ------------------------------------------------------------------------------*/
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 
@@ -23,14 +24,26 @@ namespace AlchemicalFlux.DOTS
 
         protected override void OnUpdate()
         {
-            Enabled = false;
+            Enabled = false; // Only occurs once.
 
             var spawnCubesConfig = SystemAPI.GetSingleton<SpawnCubesConfig>();
 
-            for(var iter = 0; iter < spawnCubesConfig.SpawnAmount; ++iter)
+            //for(var iter = 0; iter < spawnCubesConfig.SpawnAmount; ++iter)
+            //{
+            //    var entity = EntityManager.Instantiate(spawnCubesConfig.CubePrefabEntity);
+            //    EntityManager.SetComponentData(entity, new LocalTransform
+            //    {
+            //        Position = new(UnityEngine.Random.Range(-5f, 5f), 0, UnityEngine.Random.Range(-5f, 5f)),
+            //        Scale = 1f,
+            //        Rotation = Unity.Mathematics.quaternion.identity
+            //    });
+            //}
+
+            var outputEntities = new NativeArray<Entity>(spawnCubesConfig.SpawnAmount, Allocator.Temp);
+            EntityManager.Instantiate(spawnCubesConfig.CubePrefabEntity, outputEntities);
+            foreach(var entity in outputEntities)
             {
-                var entity = EntityManager.Instantiate(spawnCubesConfig.CubePrefabEntity);
-                EntityManager.SetComponentData(entity, new LocalTransform
+                SystemAPI.SetComponent(entity, new LocalTransform
                 {
                     Position = new(UnityEngine.Random.Range(-5f, 5f), 0, UnityEngine.Random.Range(-5f, 5f)),
                     Scale = 1f,
