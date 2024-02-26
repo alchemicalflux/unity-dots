b3a534f94b3a2939dca8e88961a35890ea089a0a
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2024-02-25 20:30:23 -0800

    feat: Add player tag and GameObject

---
+++ Assets/Scripts/PlayerAuthoring.cs
@@ -0,0 +1,34 @@
+/*------------------------------------------------------------------------------
+  File:           PlayerAuthoring.cs 
+  Project:        AlchemicalFlux Unity-Dots
+  Description:    Handles the creation and processing of the DOTS RotateSpeed
+                    component on entities.
+  Copyright:      
+
+  Last commit by: alchemicalflux 
+  Last commit at: 2024-02-25 20:30:24 
+------------------------------------------------------------------------------*/
+using Unity.Entities;
+using UnityEngine;
+
+namespace AlchemicalFlux.DOTS
+{
+
+    public class PlayerAuthoring : MonoBehaviour
+    {
+        private class Baker : Baker<PlayerAuthoring>
+        {
+            public override void Bake(PlayerAuthoring authoring)
+            {
+                // Set to dynamic due to changing rotation position.
+                var entity = GetEntity(TransformUsageFlags.None);
+                AddComponent(entity, new Player());
+            }
+        }
+    }
+
+    public struct Player : IComponentData
+    {
+        public float Value;
+    }
+}
